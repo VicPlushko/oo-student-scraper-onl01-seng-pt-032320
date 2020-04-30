5e13126ba979df76cf299de6e78ae84bbc6ff36b
Victor Plushko <vicplushko33@gmail.com>
2020-04-30 19:10:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,15 +22,14 @@ class Scraper
     html = Nokogiri::HTML(open(profile_url))
     html.css("div.social-icon-controler a").each do |student|
         url = student.attribute("href")
-        students_hash[:twitter_url] = url if url.include?("twitter")
-        students_hash[:linkedin_url] = url if url.include?("linkedin")
-        students_hash[:github_url] = url if url.include?("github")
-        students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
+        student_hash[:twitter_url] = url if url.include?("twitter")
+        student_hash[:linkedin_url] = url if url.include?("linkedin")
+        student_hash[:github_url] = url if url.include?("github")
+        student_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
     end
-        students_hash[:profile_quote] = html.css("div.profile-quote").text
-        students_hash[:bio] = html.css("div.bio-content p").text
-    students_hash
-  end
+        student_hash[:profile_quote] = html.css("div.profile-quote").text
+        student_hash[:bio] = html.css("div.bio-content p").text
+        student_hash
   end
 
 end
