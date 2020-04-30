8aebc28be80ebd10315f4e44924f06186a81ce44
Victor Plushko <vicplushko33@gmail.com>
2020-04-30 19:36:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     student_array.each do |student_hash|
-      self.new(student_hash)
+      Student.new(student_hash)
   end
 
   def add_student_attributes(attributes_hash)
