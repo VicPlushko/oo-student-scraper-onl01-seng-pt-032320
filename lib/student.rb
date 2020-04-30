91c80c7e36b08a2a1417e896cecf661416fe12b6
Victor Plushko <vicplushko33@gmail.com>
2020-04-30 19:42:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,12 +18,17 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |student_hash|
-      
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
+    self.send("profile_url=", attributes_hash[:profile_url])
   end
 
   def self.all
-    
+    @@all
   end
 end
 
