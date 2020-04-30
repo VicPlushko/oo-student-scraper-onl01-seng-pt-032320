bd1f4629de3c3c2c56df03f36a1e054bf6eb5b63
Victor Plushko <vicplushko33@gmail.com>
2020-04-30 19:01:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
       student_info = {
       name: student.css("h4.student-name").text,
       location: student.css("p.student-location").text,
-      profile_url: student.css("a").attribute("href")
+      profile_url: student.css("a").attribute("href").value
       }
       student_array << student_info
     end
