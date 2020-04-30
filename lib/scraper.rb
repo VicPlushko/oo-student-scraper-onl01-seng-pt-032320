2b91ec4f8da64d02b175d83b9e2ca51f7fd90d25
Victor Plushko <vicplushko33@gmail.com>
2020-04-30 18:50:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css(".student-card").collect do |student|
+      student_info = {
+      name: student.css("h4.student-name").text,
+      location: student.css("p.student-location").text,
+      profile_url = "http://students.learn.co/" + student.css("a").attribute("href")
+      }
+      student_array << student_info
+    end
+      student_array
   end
 
   def self.scrape_profile_page(profile_url)
